c8e8033e05ec6cc841d5dc530a5f161e893ca7a7
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2023-02-22 10:35:03 -0500

    Created JSON server for the tasks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import Button from "./Button";
 // destrutured
 // an alternative is to pass any variable the will
 // be an object then do variable.title
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("click");
-  };
-
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
